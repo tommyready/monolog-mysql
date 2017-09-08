e1348f16e5a79874f8d6b74bc1d98abedbcfd045
Tommy Ready <tommy.r.ready@gmail.com>
2017-09-08 10:57:18 -0400

    Update MysqlHandler.php

--- src/Logger/Monolog/Handler/MysqlHandler.php
+++ src/Logger/Monolog/Handler/MysqlHandler.php
@@ -29,7 +29,7 @@ class MysqlHandler extends AbstractProcessingHandler
             'level'       => $record['level'],
             'level_name'  => $record['level_name'],
             'context'     => json_encode($record['context']),
-            'remote_addr' => isset($_SERVER['REMOTE_ADDR'])     ? $_SERVER['REMOTE_ADDR'] : null,
+            'remote_addr' => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null,
             'user_agent'  => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT']      : null,
             'created_by'  => Auth::id() > 0 ? Auth::id() : null,
             'created_at'  => $record['datetime']->format('Y-m-d H:i:s')
